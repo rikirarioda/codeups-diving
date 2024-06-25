bb58743d3b7ee5a5706d6cda96141e09332626cb
riodanno <rikirarioda@gmail.com>
2024-06-26 00:21:29 +0900

    0626

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-hamburger, .js-drawer").click(function () {
       $(".js-hamburger").toggleClass("is-active");
       $(".js-drawer").fadeToggle();
+      
     });
 
     // ウィンドウのリサイズイベント
